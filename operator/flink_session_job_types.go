aa7856b76972289790fe408e35290bbd3817822c
Hao Xin <haoxinst@gmail.com>
2022-03-23 21:44:17 +0800

    Init FlinkSessionJob

---
+++ operator/flink_session_job_types.go
@@ -0,0 +1,22 @@
+package operator
+
+import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+type FlinkSessionJob struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec FlinkSessionJobSpec `json:"spec"`
+}
+
+type FlinkSessionJobSpec struct {
+	ClusterId string  `json:"clusterId"`
+	Job       JobSpec `json:"job"`
+}
+
+type JobSpec struct {
+	JarURI      string `json:"jarURI"`
+	Parallelism int32  `json:"parallelism"`
+	EntryClass  string `json:"entryClass"`
+	UpgradeMode string `json:"upgradeMode"`
+}
