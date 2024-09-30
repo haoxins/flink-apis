c1e30ae18c5d91bd8613d973bce572eb78e30168
Hao Xin <haoxinst@gmail.com>
2024-09-30 15:23:21 +0800

    chore: mod

--- v1beta1/flink_sessionjob_types_test.go
+++ v1beta1/flink_sessionjob_types_test.go
@@ -1,58 +1,57 @@
 package v1beta1
 
 import (
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-var _ = Describe("Test FlinkSessionJobSpec", func() {
-	It("FlinkSessionJobSpec Equals should work", func() {
-		j1 := &FlinkSessionJobSpec{
-			DeploymentName: "test",
-			Job: JobSpec{
-				JarURI:      "test",
-				Parallelism: 1,
-			},
-		}
-		j2 := &FlinkSessionJobSpec{
-			DeploymentName: "test",
-			Job: JobSpec{
-				JarURI:      "test",
-				Parallelism: 1,
-			},
-		}
+func Test_FlinkSessionJobSpec_Equals(t *testing.T) {
+	j1 := &FlinkSessionJobSpec{
+		DeploymentName: "test",
+		Job: JobSpec{
+			JarURI:      "test",
+			Parallelism: 1,
+		},
+	}
+	j2 := &FlinkSessionJobSpec{
+		DeploymentName: "test",
+		Job: JobSpec{
+			JarURI:      "test",
+			Parallelism: 1,
+		},
+	}
 
-		v := j1.Equals(j2)
-		Expect(v).To(BeTrue())
+	v := j1.Equals(j2)
+	assert.True(t, v)
 
-		j2.DeploymentName = "test2"
-		v = j1.Equals(j2)
-		Expect(v).To(BeFalse())
+	j2.DeploymentName = "test2"
+	v = j1.Equals(j2)
+	assert.False(t, v)
 
-		j2.DeploymentName = "test"
-		j2.Job.Parallelism = 2
-		v = j1.Equals(j2)
-		Expect(v).To(BeFalse())
+	j2.DeploymentName = "test"
+	j2.Job.Parallelism = 2
+	v = j1.Equals(j2)
+	assert.False(t, v)
 
-		j2.Job.Parallelism = 1
-		j1.Job.Args = []string{""}
-		j2.Job.Args = []string{""}
-		v = j1.Equals(j2)
-		Expect(v).To(BeTrue())
+	j2.Job.Parallelism = 1
+	j1.Job.Args = []string{""}
+	j2.Job.Args = []string{""}
+	v = j1.Equals(j2)
+	assert.True(t, v)
 
-		j1.Job.Args = []string{"--name", "a", "--topic", "b"}
-		j2.Job.Args = []string{"--name", "a", "--topic", "b"}
-		v = j1.Equals(j2)
-		Expect(v).To(BeTrue())
+	j1.Job.Args = []string{"--name", "a", "--topic", "b"}
+	j2.Job.Args = []string{"--name", "a", "--topic", "b"}
+	v = j1.Equals(j2)
+	assert.True(t, v)
 
-		j1.Job.Args = []string{"--name", "a", "--topic", "b"}
-		j2.Job.Args = []string{"--topic", "b", "--name", "a"}
-		v = j1.Equals(j2)
-		Expect(v).To(BeTrue())
+	j1.Job.Args = []string{"--name", "a", "--topic", "b"}
+	j2.Job.Args = []string{"--topic", "b", "--name", "a"}
+	v = j1.Equals(j2)
+	assert.True(t, v)
 
-		j1.Job.Args = []string{"--name", "a", "--topic", "b"}
-		j2.Job.Args = []string{"--name", "b", "--topic", "a"}
-		v = j1.Equals(j2)
-		Expect(v).To(BeFalse())
-	})
-})
+	j1.Job.Args = []string{"--name", "a", "--topic", "b"}
+	j2.Job.Args = []string{"--name", "b", "--topic", "a"}
+	v = j1.Equals(j2)
+	assert.False(t, v)
+}
